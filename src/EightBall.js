604696cf836cabab5bafcc87225c172972c34d6a
Justin Z <contactjustinzamora@gmail.com>
2021-01-20 00:09:10 -0800

    Eightball produces color and message

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,13 +1,30 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import "./EightBall.css";
 
-const EightBall = ({answers}) =>{
-    const [color, setColor] = useState('black')
-    const [message, setMessage] = useState('Think of a Question.')
-    return (
-        <>
-        {answers.map(a => {return (a.msg)})}
-        </>
-    )
+function choice(arr) {
+  const randomIdx = Math.floor(Math.random() * arr.length);
+  return arr[randomIdx];
+}
+
+function EightBall(props) {
+  const [msg, setMsg] = useState("Think of a Question.");
+  const [color, setColor] = useState("black");
+
+  function handleClick() {
+    const { msg, color } = choice(props.answers);
+    setMsg(msg);
+    setColor(color);
+  }
+
+  return (
+    <div
+      className="EightBall"
+      onClick={handleClick}
+      style={{ backgroundColor: color }}
+    >
+      <b>{msg}</b>
+    </div>
+  );
 }
 
 export default EightBall
